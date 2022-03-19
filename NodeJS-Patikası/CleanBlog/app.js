9422f6ef7a4496e01926789700a7fa61b5e5a615
Sefa Önder <sefa.onder@oymakyazilim.com>
2022-03-19 12:42:40 +0300

    nodejs homework 9 done

--- NodeJS-Patikası/CleanBlog/app.js
+++ NodeJS-Patikası/CleanBlog/app.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const path = require('path');
 const ejs = require('ejs');
+const Post = require('./models/Post');
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template Engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find();
+  res.render('index', {
+    posts,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -21,6 +37,15 @@ app.get('/about', (req, res) => {
 
 app.get('/add-post', (req, res) => {
   res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
